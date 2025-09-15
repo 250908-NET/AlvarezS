421b856ea62b2c5df3e01addb2a9b07201161b73
Salvarez99 <stephenalvarez3@gmail.com>
2025-09-15 13:52:54 -0400

    Todo.API - added Priority API

--- week1/Todo.App/Todo.API/models/Task.cs
+++ week1/Todo.App/Todo.API/models/Task.cs
@@ -4,6 +4,7 @@ public class Task
     public string title { get; set; }
     public string description { get; set; }
     public bool isCompleted { get; set; }
+    public Priority priority { get; set; }
     public DateTime dueDate { get; set; }
     public DateTime createdAt { get; set; }
     public DateTime updatedAt { get; set; }
