cd74ce907d4e76e57d76796c83fc35b6f8638c6d
Salvarez99 <stephenalvarez3@gmail.com>
2025-09-18 11:31:01 -0400

    Todo.Test - DELETE / GET Task By Id

--- week1/Todo.App/Todo.Test/UnitTest1.cs
+++ week1/Todo.App/Todo.Test/UnitTest1.cs
@@ -3,15 +3,16 @@ using FluentAssertions;
 using Xunit;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net.Http.Json;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Web.Test;
 
-public class Response
+public class Response<T>
 {
     public bool? success { set; get; }
     public string? error { set; get; }
     public string? message { set; get; }
-    public TaskItem? data { set; get; }
+    public T? data { set; get; }
 }
 
 public class ApiTest : IClassFixture<WebApplicationFactory<Program>>
@@ -21,6 +22,7 @@ public class ApiTest : IClassFixture<WebApplicationFactory<Program>>
     public ApiTest(WebApplicationFactory<Program> factory)
     {
         _client = factory.CreateClient();
+        
     }
 
     [Fact]
@@ -38,11 +40,10 @@ public class ApiTest : IClassFixture<WebApplicationFactory<Program>>
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        var created = await response.Content.ReadFromJsonAsync<Response>();
+        var created = await response.Content.ReadFromJsonAsync<Response<TaskItem>>();
         created!.success.Should().Be(true);
         created!.message.Should().Be("Task created");
-        // created!.taskItem.Should().NotBeNull();
-        // created!.taskItem!.id.Should().Be(1);
+        created!.data.Should().NotBeNull();
     }
 
     [Fact]
@@ -59,7 +60,7 @@ public class ApiTest : IClassFixture<WebApplicationFactory<Program>>
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
 
-        var created = await response.Content.ReadFromJsonAsync<Response>();
+        var created = await response.Content.ReadFromJsonAsync<Response<TaskItem>>();
         created!.success.Should().Be(false);
         created!.message.Should().Be("Title is required");
     }
@@ -69,25 +70,28 @@ public class ApiTest : IClassFixture<WebApplicationFactory<Program>>
     {
         var newTodo = new
         {
-            title = "Update title",
-            desc = "Updating TaskItem 1 in TaskService"
+            title = "Learn testing",
+            desc = "Adding TaskItem to TaskService"
         };
+        var createResponse = await _client.PostAsJsonAsync("/api/tasks", newTodo);
+        var created = await createResponse.Content.ReadFromJsonAsync<Response<TaskItem>>();
+        var id = created!.data!.id;
 
         // Act
-        var response = await _client.PutAsJsonAsync("/api/tasks/1", newTodo);
+        var response = await _client.PutAsJsonAsync($"/api/tasks/{id}", newTodo);
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        var created = await response.Content.ReadFromJsonAsync<Response>();
-        created!.success.Should().Be(true);
-        created!.message.Should().Be("Task updated");
+        var updated = await response.Content.ReadFromJsonAsync<Response<TaskItem>>();
+        updated!.success.Should().Be(true);
+        updated!.message.Should().Be("Task updated");
     }
 
     [Fact]
     public async Task FAIL_PUTUpdateTaskItem()
     {
-        
+
         var newTodo = new
         {
             title = "Update title",
@@ -100,20 +104,21 @@ public class ApiTest : IClassFixture<WebApplicationFactory<Program>>
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
 
-        var created = await response.Content.ReadFromJsonAsync<Response>();
+        var created = await response.Content.ReadFromJsonAsync<Response<TaskItem>>();
         created!.success.Should().Be(false);
         created!.message.Should().Be("Task not found or update failed");
     }
-    
+
     [Fact]
     public async Task PASS_DELETETaskItem()
     {
         //Arrange
-        var createResponse = await _client.PostAsJsonAsync("/api/tasks", new {
+        var createResponse = await _client.PostAsJsonAsync("/api/tasks", new
+        {
             title = "Temp Task",
             desc = "To be deleted"
         });
-        var created = await createResponse.Content.ReadFromJsonAsync<Response>();
+        var created = await createResponse.Content.ReadFromJsonAsync<Response<TaskItem>>();
         var id = created!.data!.id;
 
         // Act
@@ -122,8 +127,96 @@ public class ApiTest : IClassFixture<WebApplicationFactory<Program>>
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        var deleted = await response.Content.ReadFromJsonAsync<Response>();
+        var deleted = await response.Content.ReadFromJsonAsync<Response<TaskItem>>();
         deleted!.success.Should().Be(true);
         deleted!.message.Should().Be("Operation completed successfully");
+    }
+
+    [Fact]
+    public async Task FAIL_DELETETaskItem()
+    {
+        //Arrange
+        //No Tasks in list
+
+        // Act
+        var response = await _client.DeleteAsync($"/api/tasks/999999");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var deleted = await response.Content.ReadFromJsonAsync<Response<TaskItem>>();
+        deleted!.success.Should().Be(false);
+        deleted!.message.Should().Be("Operation failed");
+        deleted!.error.Should().Be("Id not found");
+    }
+
+    [Fact]
+    public async Task PASS_GETTaskItemById()
+    {
+        //Arrange
+        var addTask = await _client.PostAsJsonAsync("/api/tasks", new
+        {
+            title = "Get this task",
+            desc = "Unit test to get a task by id"
+        });
+        var created = await addTask.Content.ReadFromJsonAsync<Response<TaskItem>>();
+        var id = created!.data!.id;
+
+        // Act
+        var data = await _client.GetAsync($"/api/tasks/{id}");
+
+        // Assert
+        data.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var task = await data.Content.ReadFromJsonAsync<Response<TaskItem>>();
+        task!.success.Should().Be(true);
+        task!.message.Should().Be("Operation completed successfully");
+        task!.data!.id.Should().Be(id);
+        task!.data!.title.Should().Be("Get this task");
+    }
+
+    [Fact]
+    public async Task FAIL_GETTaskItemById()
+    {
+        //Arrange
+        //No Tasks in list
+
+        // Act
+        var data = await _client.GetAsync($"/api/tasks/99999");
+
+        // Assert
+        data.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var task = await data.Content.ReadFromJsonAsync<Response<TaskItem>>();
+        task!.success.Should().Be(false);
+        task!.message.Should().Be("Operation failed");
+        task!.error.Should().Be("Id not found");
+    }
+
+        [Fact]
+    public async Task PASS_GETTaskItemList()
+    {
+        //Arrange
+        Random r = new Random();
+        for (int i = 50; i < 70; i++)
+        {
+            await _client.PostAsJsonAsync("/api/tasks", new
+            {
+                title = $"Task {i}",
+                priority = r.Next(0, 4),
+                dueDate = $"2025-06-{i}"
+            });
+
+        }
+
+        // Act
+        var data = await _client.GetAsync($"/api/tasks");
+
+        // Assert
+        data.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var task = await data.Content.ReadFromJsonAsync<Response<List<TaskItem>>>();
+        task!.success.Should().Be(true);
+        task!.message.Should().Be("Operation completed successfully");
     }
 }
