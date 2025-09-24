136bc28b6bed3e380bad23544eae9ca612cb9c4b
Salvarez99 <stephenalvarez3@gmail.com>
2025-09-24 14:39:35 -0400

    Set default values for model constructors

--- week2/Project 1/EventManager.App/EventManager.API/models/Attendee.cs
+++ week2/Project 1/EventManager.App/EventManager.API/models/Attendee.cs
@@ -6,16 +6,16 @@ public class Attendee
     public int Id { get; set; }
 
     [Required]
-    public string FirstName { get; set; }
+    public string FirstName { get; set; } = "";
 
     [Required]
-    public string LastName { get; set; }
+    public string LastName { get; set; } = "";
 
     [Required, EmailAddress]
-    public string Email { get; set; }
+    public string Email { get; set; } = "";
 
     [Required, Phone]
-    public string Phone { get; set; }
+    public string Phone { get; set; } = "";
 
     public List<Event> Events { get; set; } = new();
     
--- week2/Project 1/EventManager.App/EventManager.API/models/Event.cs
+++ week2/Project 1/EventManager.App/EventManager.API/models/Event.cs
@@ -7,13 +7,13 @@ public class Event
     public int Id { get; set; }
 
     [Required, MaxLength(50)]
-    public string Title { get; set; }
+    public string Title { get; set; } = "";
 
     [Required, MaxLength(100)]
-    public string Description { get; set; }
+    public string Description { get; set; } = "";
 
     [Required, MaxLength(200)]
-    public string Location { get; set; }
+    public string Location { get; set; } = "";
 
     [Required]
     public DateTime StartDateTime { get; set; }
